a87746b7291ea21e1a56a7211bce53bb690b7aae
popkid <devpopkid@gmail.com>
2026-03-19 16:18:31 +0300

    Create config.cjs

---
+++ config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+    PREFIX: '.',
+    MODE: 'public',
+    SESSION_ID: 'POPKID~PASTE_YOUR_ID_HERE', // This will now decode automatically
+    AUTO_STATUS_SEEN: true,
+    OWNER_NUMBER: '254700000000'
+};
