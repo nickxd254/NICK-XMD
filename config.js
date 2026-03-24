5a1d5b473afb51c97bd0b9966a5bb1832fb8f99b
popkid <popkidmdbot@gmail.com>
2026-03-24 19:33:43 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,8 @@ module.exports = {
     PREFIX: process.env.PREFIX || ".",
     MODE: process.env.MODE || "public",
     AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+    AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+    AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+    AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "Seen by NICK-XMD 🤖",
     AUTO_BIO: process.env.AUTO_BIO || "true",
 };
