2be643ad29fb2bef87c3ca8e4aedc7405b21f637
popkidke <popxmd@gmail.com>
2026-03-28 11:36:12 +0300

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "POPKID;;;Your_Mega_ID_Here",
-    PREFIX: process.env.PREFIX || ".",
-    MODE: process.env.MODE || "public",
-    AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
-    AUTO_BIO: process.env.AUTO_BIO || "true",
-};
