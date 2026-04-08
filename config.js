763c70a2099bbfe12e208a24f726b482882efc18
popkidke <popxmd@gmail.com>
2026-04-08 23:44:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "",
+    SESSION_ID: process.env.SESSION_ID || "NICK;;;qro3TC6D#uortOofaZMfPqjMb87KZO3U8S-va-blfqKblpvoXKPM",
     AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
     AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
     AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
